8825802972129e47c788c1984f21b5ed32ee601e
akalakkad <advait.kalakkad@gmail.com>
2019-06-24 18:15:59 -0700

    Added player UI

--- venv/static/Scripts/index.js
+++ venv/static/Scripts/index.js
@@ -3,22 +3,30 @@ import {login, reqSong} from './song.js';
 
 document.addEventListener('DOMContentLoaded', e => {
   let token = '';
-  login.then(result => {
-    token = result;
-    query();
+  let player = new Audio();
 
+
+  login.then(result => {
     let b = document.querySelector('button');
     let recs = document.getElementsByClassName('rec-img');
 
+    token = result;
+    query();
+
+
     for(let r of recs) {
       r.addEventListener('click', e => {
+
         let current = document.getElementById('current');
         current.src = e.target.src;
         current.alt = e.target.alt;
         current.setAttribute("data-uri", e.target.getAttribute("data-uri"));
 
-        reqSong(e.target.getAttribute("data-uri").split(':')[2], token);
-
+        let song = reqSong(e.target.getAttribute("data-uri").split(':')[2], token);
+        song.then(result => {
+          player.src = result;
+          player.play();
+        });
 
         let data = {index: e.target.alt};
         suggest(data);
